a462b93df95cced1eddd88a59b6e700e47e40390
Ahmed Belal <ahmbelal95@gmail.com>
2021-08-27 16:29:03 +0200

    initial commit - basic setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require('cors')
+require("colors");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Enable CORS
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+// console.log(process.env)
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
