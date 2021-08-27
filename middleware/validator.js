112ad1e30e70613cda6672a58df854ee2d71b4b8
Ahmed Belal <ahmbelal95@gmail.com>
2021-08-28 01:46:14 +0200

    fix: name field instead of username

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 //Register Validation
 const register = Joi.object({
-  username: Joi.string().min(1).max(60).required(),
+  name: Joi.string().min(1).max(60).required(),
   email: Joi.string().min(5).max(255).email().required(),
   password: Joi.string().min(6).max(255).required(),
 });
